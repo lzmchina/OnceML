5df35e1f1a63cb2db811441624d75a022639ab25
lzm <lzm6@foxmail.com>
2021-05-26 19:19:33 +0800

    更新镜像运行命令

--- src/onceml/orchestration/kubeflow/kfp_config.py
+++ src/onceml/orchestration/kubeflow/kfp_config.py
@@ -3,8 +3,8 @@ import onceml.global_config as global_config
 import os
 # 包含镜像的一些信息
 IMAGE = 'liziming/{}:0.0.1'.format(global_config.project_name)
-COMMAND = 'python3 -m {}.orchestration.kubeflow.container_entrypoint'.format(
-    global_config.project_name)
+COMMAND = ['python3','-m','{}.orchestration.kubeflow.container_entrypoint'.format(
+    global_config.project_name)]
 WORKINGDIR = '/project'  # 容器里代码工程被挂载的目录，会在这里面执行命令
 OUTPUTSDIR = 'kfpoutputs'  # kfp输出目录，每个组件的输出会保存在这
 
