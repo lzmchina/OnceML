0a58f2cc96726834cafcdd3038befa56840def89
lzm <lzm6@foxmail.com>
2021-11-11 14:15:15 +0800

    暂时就搞搞nfs的io吧

--- benchmarks/IO/NFS/singleNode.py
+++ benchmarks/IO/NFS/singleNode.py
@@ -1,5 +1,4 @@
 import cv2
-from tqdm import tqdm_notebook as tqdm
 import zipfile
 import io
 import pandas as pd
@@ -14,12 +13,14 @@ HEIGHT = 137
 WIDTH = 236
 SIZE = 128
 
-TRAIN = ['/kaggle/input/bengaliai-cv19/train_image_data_0.parquet',
-         '/kaggle/input/bengaliai-cv19/train_image_data_1.parquet',
-         '/kaggle/input/bengaliai-cv19/train_image_data_2.parquet',
-         '/kaggle/input/bengaliai-cv19/train_image_data_3.parquet']
+TRAIN = ['/nfs-storage/default-nfstest-pvc-a83881ec-36dd-4888-ad19-7dcebda55145/train_image_data_0.parquet',
+         '/nfs-storage/default-nfstest-pvc-a83881ec-36dd-4888-ad19-7dcebda55145/train_image_data_1.parquet',
+         '/nfs-storage/default-nfstest-pvc-a83881ec-36dd-4888-ad19-7dcebda55145/train_image_data_2.parquet',
+         '/nfs-storage/default-nfstest-pvc-a83881ec-36dd-4888-ad19-7dcebda55145/train_image_data_3.parquet']
 
 OUT_TRAIN = 'train.zip'
+
+
 def bbox(img):
     rows = np.any(img, axis=1)
     cols = np.any(img, axis=0)
@@ -27,49 +28,57 @@ def bbox(img):
     cmin, cmax = np.where(cols)[0][[0, -1]]
     return rmin, rmax, cmin, cmax
 
+
 def crop_resize(img0, size=SIZE, pad=16):
-    #crop a box around pixels large than the threshold 
-    #some images contain line at the sides
-    ymin,ymax,xmin,xmax = bbox(img0[5:-5,5:-5] > 80)
-    #cropping may cut too much, so we need to add it back
+    # crop a box around pixels large than the threshold
+    # some images contain line at the sides
+    ymin, ymax, xmin, xmax = bbox(img0[5:-5, 5:-5] > 80)
+    # cropping may cut too much, so we need to add it back
     xmin = xmin - 13 if (xmin > 13) else 0
     ymin = ymin - 10 if (ymin > 10) else 0
     xmax = xmax + 13 if (xmax < WIDTH - 13) else WIDTH
     ymax = ymax + 10 if (ymax < HEIGHT - 10) else HEIGHT
-    img = img0[ymin:ymax,xmin:xmax]
-    #remove lo intensity pixels as noise
+    img = img0[ymin:ymax, xmin:xmax]
+    # remove lo intensity pixels as noise
     img[img < 28] = 0
-    lx, ly = xmax-xmin,ymax-ymin
-    l = max(lx,ly) + pad
-    #make sure that the aspect ratio is kept in rescaling
+    lx, ly = xmax-xmin, ymax-ymin
+    l = max(lx, ly) + pad
+    # make sure that the aspect ratio is kept in rescaling
     img = np.pad(img, [((l-ly)//2,), ((l-lx)//2,)], mode='constant')
-    return cv2.resize(img,(size,size))
-load_total_time=0.0
-process_total_time=0.0
-save_total_time=0.0
-with zipfile.ZipFile(OUT_TRAIN, 'w') as img_out:
-    for fname in TRAIN:
-        print(fname,flush=True)
-        load_before=time.time()
-        df = pd.read_parquet(fname)
-        load_total_time+=time.time()-load_before
-        #the input is inverted
-        process_before=time.time()
-        data = 255 - df.iloc[:, 1:].values.reshape(-1, HEIGHT, WIDTH).astype(np.uint8)
-        process_total_time+=time.time()-process_before
-        for idx in tqdm(range(len(df))):
-            name = df.iloc[idx,0]
-            #normalize each image by its max val
-            process_before=time.time()
-            img = (data[idx]*(255.0/data[idx].max())).astype(np.uint8)
-            img = crop_resize(img)
-            img = cv2.imencode('.png',img)[1]
-            process_total_time+=time.time()-process_before
-            save_before=time.time()
-            img_out.writestr(name + '.png', img)
-            save_total_time+=time.time()-save_before
+    return cv2.resize(img, (size, size))
+
+
+load_total_time = 0.0
+process_total_time = 0.0
+save_total_time = 0.0
+df_list = []
+#with zipfile.ZipFile(OUT_TRAIN, 'w') as img_out:
+load_before = time.time()
+for fname in TRAIN:
+    print(fname, flush=True)
+    df = pd.read_parquet(fname)
+    df_list.append(df)
+load_total_time = time.time()-load_before
+# the input is inverted
+process_before = time.time()
+for df in df_list:
+    data = 255 - df.values[:, 1:].reshape(-1, HEIGHT, WIDTH).astype(np.uint8)
+    for idx in range(len(df)):
+        #name = df.iloc[idx,0]
+        # normalize each image by its max val
+        # process_before=time.time()
+        img = (data[idx]*(255.0/data[idx].max())).astype(np.uint8)
+        img = crop_resize(img)
+        img = cv2.imencode('.png', img)[1]
+process_total_time = time.time()-process_before
+# save_before=time.time()
+#img_out.writestr(name + '.png', img)
+# save_total_time+=time.time()-save_before
+print(load_total_time)
+print(process_total_time)
+print(save_total_time)
 json.dump({
-    "load_time":load_total_time,
-    "process_time":process_total_time,
-    "save_time":save_total_time
-},open("./singleNodeNFS.json"),indent=4)
+    "load_time": load_total_time,
+    "process_time": process_total_time,
+    "save_time": save_total_time
+}, open("./singleNodeNFS.json","w"), indent=4)
--- benchmarks/IO/clusterFS/readme.md
+++ benchmarks/IO/clusterFS/readme.md
@@ -1,3 +1,84 @@
 查看教程：https://jimmysong.io/kubernetes-handbook/practice/using-glusterfs-for-persistent-storage.html
+https://developer.aliyun.com/article/618056
 
 https://www.cnblogs.com/ssgeek/p/11725648.html#311%E3%80%81%E6%89%80%E6%9C%89%E8%8A%82%E7%82%B9%E5%AE%89%E8%A3%85glusterfs%E5%AE%A2%E6%88%B7%E7%AB%AF
+
+有个包需要手动下载：https://packages.efficios.com/rhel/7/x86_64/old/userspace-rcu/0.10/
+https://packages.efficios.com/rhel/7/x86_64/old/userspace-rcu/0.10/userspace-rcu-0.10.0-1.el7.x86_64.rpm
+1. sudo yum update
+2. sudo yum install centos-release-gluster9
+3. sudo yum install -y glusterfs glusterfs-server 
+4. sudo yum install -y glusterfs-fuse glusterfs-rdma
+5. sudo systemctl start glusterd.service && systemctl enable glusterd.service
+
+主节点上（n167）：
+1. sudo gluster peer probe n170
+2. sudo gluster volume create k8s-volume  n167:/data/glusterfs-storage n168:/data/glusterfs-storage n169:/data/glusterfs-storage n170:/data/glusterfs-storage force
+3. sudo gluster volume start k8s-volume      sudo gluster volume stop k8s-volume sudo gluster volume delete k8s-volume 
+4. sudo gluster volume info
+
+现在gluster集群配置好了
+
+下一步安装heketi
+
+参考链接：https://computingforgeeks.com/configure-kubernetes-dynamic-volume-provisioning-with-heketi-glusterfs/
+1. 下载heketi-v10.4.0-release-10.linux.amd64.tar.gz
+2. 将解压出来的heketi、heketi-cli放在可执行目录里sudo cp heketi/{heketi,heketi-cli} /usr/local/bin
+3. sudo groupadd --system heketi
+4. sudo useradd -s /sbin/nologin --system -g heketi heketi
+5. sudo mkdir -p /var/lib/heketi /etc/heketi /var/log/heketi
+6. sudo cp heketi/heketi.json /etc/heketi
+7. 再编辑/etc/heketi/heketi.json文件
+8.  sudo ssh-keygen -f /etc/heketi/heketi_key -t rsa -N ''
+9.  sudo chown heketi:heketi /etc/heketi/heketi_key*
+8. sudo vim /etc/systemd/system/heketi.service
+
+9. sudo cp heketi.env /etc/heketi/heketi.env
+
+10. sudo chown -R heketi:heketi /var/lib/heketi /var/log/heketi /etc/heketi
+11. sudo systemctl daemon-reload
+12.  sudo systemctl start heketi
+13.  sudo systemctl status heketi
+14.  heketi-cli topology load --user admin --secret 123456 --json=/home/lzm/OnceML/benchmarks/IO/clusterFS/heketi-client/share/heketi/topology-sample.json
+15. 再创建storage-class: kubectl apply -f benchmarks/IO/clusterFS/storage-class.yaml
+16. 测试一下：kubectl apply -f benchmarks/IO/clusterFS/glusterfs-pvc.yaml
+    kubectl apply -f benchmarks/IO/clusterFS/gluster-pod.yaml
+
+
+
+太麻烦了，还是用k8s的方式部署
+
+1. cat heketi-service-account.json 
+2. kubectl apply -f heketi-service-account.json 
+3. kubectl get sa
+
+   kubectl apply -f deploy-heketi-clusterrole.yaml
+   kubectl apply -f deploy-heketi-gluster-admin.yaml
+
+5. kubectl create secret generic heketi-config-secret --from-file=./heketi.json 
+6. kubectl create -f heketi-bootstrap.json 
+7. kubectl apply -f heketi-deployment.json(这一步可以放在第9步后面执行)
+8. kubectl get pods
+9. 现在开始创建gfs集群，，要使用上面的heketi-cli
+    - 首先修改topology-sample.json文件，目的是让heketi发现gluster集群
+    - 获取deploy-heketi的clutserIP： kubectl get svc|grep heketi
+    - kubectl port-forward deploy-heketi-6c687b4b84-l7lws 8080:8080进行端口转发
+    - 测试端口 curl http://127.0.0.1:8080/hello
+    - 最后，为Heketi CLI客户端设置一个环境变量，以便它知道Heketi服务器的地址。
+        export HEKETI_CLI_SERVER=http://127.0.0.1:8080
+    - 再将拓扑结构提交上去heketi-cli --user admin --secret 'My Secret' topology load --json=topology-sample.json
+失败了。。。。。。
+
+
+-----
+静态pv的操作：
+见https://jimmysong.io/kubernetes-handbook/practice/using-glusterfs-for-persistent-storage.html
+1. kubectl apply -f benchmarks/IO/clusterFS/gluster-endpoints.yaml
+2. kubectl apply -f benchmarks/IO/clusterFS/gluster-svc.yaml
+2. kubectl apply -f benchmarks/IO/clusterFS/gluster-pod.yaml
+
+上面测试了下，发现是正常的，下面创建pv、pvc
+1. kubectl apply -f benchmarks/IO/clusterFS/gluster-pv.yaml
+2. kubectl apply -f benchmarks/IO/clusterFS/glusterfs-pvc.yaml
+
+kubectl apply -f benchmarks/IO/clusterFS/job.yaml
